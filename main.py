5655c85dfd723d18cd28580a2135960215bf9215
Andrew <97779449+vAPGHv@users.noreply.github.com>
2022-05-22 11:47:09 +0300

    Update and rename ImageConverter.py to main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from PIL import Image as img
+from colorama import Fore, Back, init
+
+init() # init with colorama
+
+
+try:
+    print(Fore.GREEN + Back.MAGENTA, end="")
+    ico = img.open(input(
+        "Enter filename: "
+    ))
+
+    print(Fore.MAGENTA + Back.CYAN, end="")
+    ico.save(input(
+        "Enter new name: "
+    ))
+
+except Exception as ex:
+    print(Fore.BLACK + Back.RED, end="")
+    print(f"Oops, error: \"{ex}\"")
